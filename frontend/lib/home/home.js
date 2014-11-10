49ab07ad244c0ef3b26f96e42dd2839390237bda
Martin Gontovnikas <martin@gon.to>
2014-11-10 10:21:00 +0100

    Moving forward with the sample

--- frontend/lib/home/home.js
+++ frontend/lib/home/home.js
@@ -1,10 +1,30 @@
+var request = require('browser-request')
+var jwtDecode = require('jwt-decode');
+
 var html = require('./home.html');
 var templater = require('../template');
 var Homepage = templater(html);
 
-Homepage.prototype.callAnonymousApi = function() {
+Homepage.prototype.callApi = function(type, url) {
+  var _this = this;
+  this.set('response', null);
+  this.set('api', type);
+  request({
+    url: url,
+    method: 'GET'
+  }, function(err, response, body) {
+    _this.set('response', body);
+  });
+}
 
+Homepage.prototype.callAnonymousApi = function() {
+  this.callApi('Anonymous', 'http://localhost:3001/api/random-quote');
 };
+
+Homepage.prototype.callSecuredApi = function() {
+  this.callApi('Secured', 'http://localhost:3001/api/protected/random-quote');
+}
+
 
 var jwt = localStorage.getItem('jwt');
 var decodedJwt = jwt && jwtDecode(jwt);
