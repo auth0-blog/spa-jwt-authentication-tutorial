5b51577190a957b977e3c9869638227c652d0523
Martin Gontovnikas <martin@gon.to>
2015-07-08 00:01:43 -0700

    Moving things around

--- backend/anonymous-routes.js
+++
@@ -1,8 +0,0 @@
-var express = require('express'),
-    quoter  = require('./quoter');
-
-var app = module.exports = express.Router();
-
-app.get('/api/random-quote', function(req, res) {
-  res.status(200).send(quoter.getRandomOne());
-});
